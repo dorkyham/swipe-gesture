deefd95e57a97ece51be27bd6f212144fde36015
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-07-11 14:17:14 +0700

    Initial Commit

---
+++ Belajar_Gesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Belajar_Gesture
+//
+//  Created by Annisa Nabila Nasution on 11/07/19.
+//  Copyright © 2019 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

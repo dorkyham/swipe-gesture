1f2bb3ea5cbd7527987c64541318ab45024954f7
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-07-11 16:27:47 +0700

    initial commit

--- Belajar_Gesture/ViewController.swift
+++ Belajar_Gesture/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
+    
+    @IBOutlet var rect: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //ke kanan
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipped(_:)))
+        rightSwipe.direction = .right
+        
+        //ke kiri
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipped(_:)))
+        leftSwipe.direction = .left
+        
+        //ke atas
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipped(_:)))
+        upSwipe.direction = .up
+        
+        //ke bawah
+        let bottomSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipped(_:)))
+        bottomSwipe.direction = .down
+        
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(bottomSwipe)
     }
 
+
+    @IBAction func tapped(_ sender: UITapGestureRecognizer) {
+        rect.backgroundColor = .blue
+    }
+    
+    
+    
+    @IBAction func swipped(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended{
+            switch sender.direction {
+            case .right:
+                rect.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+                break
+            case .left:
+                rect.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+                break
+            case .up:
+                rect.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+                break
+            case .down:
+                rect.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+                break
+            default:
+                break
+            }
+        }
+    }
 
 }
 
